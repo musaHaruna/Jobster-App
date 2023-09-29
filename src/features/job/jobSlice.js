fee9b8071f188bbd3118df2cf711288d57308ece
musaHaruna <124810379+musaHaruna@users.noreply.github.com>
2023-09-29 10:14:32 -0700

    job-slice-create-job-reducer

--- src/features/job/jobSlice.js
+++ src/features/job/jobSlice.js
@@ -1,7 +1,8 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
-import { toast } from 'react-toastify'
-import customFetch from '../../utils/axios'
-import { getUserFromLocalStorage } from '../../utils/localStorage'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { toast } from 'react-toastify';
+import customFetch from '../../utils/axios';
+import { getUserFromLocalStorage } from '../../utils/localStorage';
+import { logoutUser } from '../user/userSlice';
 
 const initialState = {
   isLoading: false,
@@ -14,21 +15,55 @@ const initialState = {
   status: 'pending',
   isEditing: false,
   editJobId: '',
-}
+};
+
+export const createJob = createAsyncThunk(
+  'job/createJob',
+  async (job, thunkAPI) => {
+    try {
+      const resp = await customFetch.post('/jobs', job, {
+        headers: {
+          authorization: `Bearer ${thunkAPI.getState().user.user.token}`,
+        },
+      });
+      thunkAPI.dispatch(clearValues());
+      return resp.data;
+    } catch (error) {
+      if (error.response.status === 401) {
+        thunkAPI.dispatch(logoutUser());
+        return thunkAPI.rejectWithValue('Unauthorized! Logging Out...');
+      }
+      return thunkAPI.rejectWithValue(error.response.data.msg);
+    }
+  }
+);
 
 const jobSlice = createSlice({
   name: 'job',
   initialState,
   reducers: {
     handleChange: (state, { payload: { name, value } }) => {
-      state[name] = value
+      state[name] = value;
     },
     clearValues: () => {
-      return initialState
+      return initialState;
     },
   },
-})
+  extraReducers: {
+    [createJob.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [createJob.fulfilled]: (state) => {
+      state.isLoading = false;
+      toast.success('Job Created');
+    },
+    [createJob.rejected]: (state, { payload }) => {
+      state.isLoading = false;
+      toast.error(payload);
+    },
+  },
+});
 
-export const { handleChange, clearValues } = jobSlice.actions
+export const { handleChange, clearValues } = jobSlice.actions;
 
-export default jobSlice.reducer
+export default jobSlice.reducer;
