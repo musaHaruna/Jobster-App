6b020f9c44b42a1a4a9872445781ffbd242a734e
musaHaruna <124810379+musaHaruna@users.noreply.github.com>
2023-09-29 11:22:33 -0700

    axios-interceptors

--- src/features/job/jobThunk.js
+++ src/features/job/jobThunk.js
@@ -3,17 +3,9 @@ import customFetch from '../../utils/axios';
 import { clearValues } from './jobSlice';
 import { logoutUser } from '../user/userSlice';
 
-const authHeader = (thunkAPI) => {
-  return {
-    headers: {
-      authorization: `Bearer ${thunkAPI.getState().user.user.token}`,
-    },
-  };
-};
-
 export const createJobThunk = async (job, thunkAPI) => {
   try {
-    const resp = await customFetch.post('/jobs', job, authHeader(thunkAPI));
+    const resp = await customFetch.post('/jobs', job);
     thunkAPI.dispatch(clearValues());
     return resp.data.msg;
   } catch (error) {
@@ -27,11 +19,7 @@ export const createJobThunk = async (job, thunkAPI) => {
 export const deleteJobThunk = async (jobId, thunkAPI) => {
   thunkAPI.dispatch(showLoading());
   try {
-    const resp = await customFetch.delete(`/jobs/${jobId}`, {
-      headers: {
-        authorization: `Bearer ${thunkAPI.getState().user.user.token}`,
-      },
-    });
+    const resp = await customFetch.delete(`/jobs/${jobId}`);
     thunkAPI.dispatch(getAllJobs());
     return resp.data.msg;
   } catch (error) {
@@ -41,11 +29,7 @@ export const deleteJobThunk = async (jobId, thunkAPI) => {
 };
 export const editJobThunk = async ({ jobId, job }, thunkAPI) => {
   try {
-    const resp = await customFetch.patch(`/jobs/${jobId}`, job, {
-      headers: {
-        authorization: `Bearer ${thunkAPI.getState().user.user.token}`,
-      },
-    });
+    const resp = await customFetch.patch(`/jobs/${jobId}`, job);
     thunkAPI.dispatch(clearValues());
     return resp.data;
   } catch (error) {
